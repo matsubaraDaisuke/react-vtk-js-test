350cd3ea015a9f67b54b1bcaf20e877070b329dd
matsubara <matsunenekao@gmail.com>
2021-11-19 00:08:07 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor";
 import { View, GeometryRepresentation, Reader } from "react-vtk-js";
 
 // React complains about unique key prop but I don't see why
 function Example(props) {
-  const actor = vtkActor.newInstance();
-  actor.getProperty().setEdgeVisibility(true);
-
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <View>
